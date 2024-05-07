9b75c3b9945df188bc6666898761e51adfa4a3ba
AhmadSharafat <112656643+AhmadSharafat@users.noreply.github.com>
2024-05-07 21:49:54 +0500

    Add files via upload

--- script.js
+++ script.js
@@ -1,61 +1,88 @@
-'use strict';
-// console.log(document.querySelector('.message').textContent);
-// document.querySelector('.message').textContent = 'Correct Number :)';
-// document.querySelector('.number').textContent = '$';
-// document.querySelector('.guess').value = 24;
-// // console.log(document.querySelector('.guess').value);
+"use strict";
+// Selecting Element
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
-let score = 20;
-let highScore = 0;
+const score0El = document.querySelector("#score--0");
+const score1El = document.getElementById("score--1");
+const current0El = document.getElementById("current--0");
+const current1El = document.getElementById("current--1");
+const dicEl = document.querySelector(".dice");
+const btnnew = document.querySelector(".btn--new");
+const btnroll = document.querySelector(".btn--roll");
+const btnhold = document.querySelector(".btn--hold");
 
-const secretNumber = Math.trunc(Math.random() * 20) + 1;
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+dicEl.classList.add("hidden");
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
+};
+// Buttons Functionality
+btnroll.addEventListener("click", function () {
+  if (playing) {
+    // 1.Generating random dice roll
 
-document.querySelector('.check').addEventListener('click', function () {
-    const guess = Number(document.querySelector('.guess').value);
+    const dice = Math.trunc(Math.random() * 6) + 1;
+    // 2. Display a dice
+    dicEl.classList.remove("hidden");
+    dicEl.src = `dice-${dice}.png`;
+    // Check for rolled 1
+    if (dice !== 1) {
+      // Add dice to current score
 
-
-    // when there is no input
-
-    if (! guess) {
-        document.querySelector('.message').textContent = 'no number';
-        // when the player wins the game
-    } else if (guess === secretNumber) {
-        document.querySelector('.message').textContent = 'Correct Number';
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
-        document.querySelector('.number').textContent = secretNumber;
-        if (score > highScore) {
-            highScore = score;
-            document.querySelector('.highscore').textContent = highScore;
-        }
-        // when the number is too high
-    } else if (guess !== secretNumber) {
-        if (score > 1) {
-            document.querySelector('.message').textContent = guess > secretNumber ? 'TOO High ' : 'TOO Low';
-            score--;
-            document.querySelector('.score').textContent = score;
-
-            // when the player lose the game
-        } else {
-            document.querySelector('.message').textContent = 'you lost the game';
-            document.querySelector('.score').textContent = 0;
-        }
-        // when the score is too low
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch the player
+      switchPlayer();
     }
+  }
+});
 
-})
+btnhold.addEventListener("click", function () {
+  if (playing) {
+    //1. Add current score to acive player's score
+    scores[activePlayer] += currentScore;
+    // scores[1]=scores[1]+currentScore
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+    //2 too End the game
+    if (scores[activePlayer] >= 20) {
+      dicEl.classList.add("hidden");
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add("player--winner");
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove("player--active");
+    } else {
+      switchPlayer();
+    }
+  }
+});
 
-
-// Function of again button
-
-document.querySelector('.again').addEventListener('click', function () {
-    let score = 20;
-    const secretNumber = Math.trunc(Math.random() * 20) + 1;
-    document.querySelector('.score').textContent = score;
-    document.querySelector('.number').textContent = '?';
-    document.querySelector('.message').textContent = 'Start guessing...';
-    document.querySelector('.guess').textContent = '';
-    document.querySelector('body').style.backgroundColor = '#222';
-
-})
+btnnew.addEventListener("click", function () {
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  dicEl.classList.add("hidden");
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove("player--winner");
+});
